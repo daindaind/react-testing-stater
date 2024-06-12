04744da32c79df8210f58528bd64305977c39b2b
chaKK <cdin23@naver.com>
2024-06-12 16:53:08 +0900

    강의 초기 세팅

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import axios from "axios";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import "./index.css";
+import routes from "./routes";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+axios.defaults.baseURL = "http://localhost:3000";
+
+const router = createBrowserRouter(routes);
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
